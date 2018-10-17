0e4119e4758a864a263b4ef260b42b13cf1225a9
jimmyb63 <c3215105@uon.edu.au>
2018-10-17 22:20:50 +1100

    pagination

--- SmartWay/SmartWay/UL/Views/ViewAdvertisementList.aspx.designer.cs
+++ SmartWay/SmartWay/UL/Views/ViewAdvertisementList.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace SmartWay.UL.Views {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListView adList;
+        
+        /// <summary>
+        /// DataPagerAds control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataPager DataPagerAds;
     }
 }
