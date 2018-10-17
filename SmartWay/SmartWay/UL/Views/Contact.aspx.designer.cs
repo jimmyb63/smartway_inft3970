30bdbc9012a157a4f86d067e30ec17f6d55c5d86
Gabby <kreepy_kitty@live.co.uk>
2018-10-18 10:28:18 +1100

    Contact.aspx formatting
    IN APP:
    - Contact.aspx formatted
    - Added ValidationSummary to display errors
    - Changed error messages
    - All fields are required

--- SmartWay/SmartWay/UL/Views/Contact.aspx.designer.cs
+++ SmartWay/SmartWay/UL/Views/Contact.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace SmartWay.UL.Views {
     public partial class Contact {
         
         /// <summary>
+        /// ValidationSummaryContact control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummaryContact;
+        
+        /// <summary>
         /// txtFirstName control.
         /// </summary>
         /// <remarks>
