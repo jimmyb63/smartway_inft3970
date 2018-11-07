6cbe7c4bb8bee9943ffd0fca5e9643521913210c
Gabby <kreepy_kitty@live.co.uk>
2018-11-07 11:39:03 +1100

    Formatting in ReplyToMessage
    IN APP:
    - Small formatting and alignment of ReplyToMessage.aspx

--- SmartWay/SmartWay/UL/Views/ReplyToMessage.aspx.designer.cs
+++ SmartWay/SmartWay/UL/Views/ReplyToMessage.aspx.designer.cs
@@ -49,15 +49,6 @@ namespace SmartWay.UL.Views {
         protected global::System.Web.UI.WebControls.HiddenField hfpmChainID;
         
         /// <summary>
-        /// lblAdDetails control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblAdDetails;
-        
-        /// <summary>
         /// lblAdTitle control.
         /// </summary>
         /// <remarks>
